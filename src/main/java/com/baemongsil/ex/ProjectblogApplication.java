d32ccb0c28d195f856ec6a1db3b048052f8cb72f
배준희 <>
2022-04-07 17:35:14 +0900

    22.04.07

---
+++ src/main/java/com/baemongsil/ex/ProjectblogApplication.java
@@ -0,0 +1,13 @@
+package com.baemongsil.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectblogApplication.class, args);
+	}
+
+}
