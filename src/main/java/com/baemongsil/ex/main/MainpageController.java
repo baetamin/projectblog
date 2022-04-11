85a844e2c1f46b22bdc05d1e5d8e42984abe8b3b
배준희 <>
2022-04-11 16:33:06 +0900

    220411

---
+++ src/main/java/com/baemongsil/ex/main/MainpageController.java
@@ -0,0 +1,18 @@
+package com.baemongsil.ex.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
+public class MainpageController {
+
+	
+	@GetMapping("/mainpage")
+	public String main() {
+		return "main/mainpage";
+	}
+	
+	
+}
